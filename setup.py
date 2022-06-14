b350970570eb374afacde8c65a62969e64e73e24
sachinkmr2468 <nihcas.kmr@gmail.com>
2022-06-14 09:53:21 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="incomeprediction",
+    version="0.0.3",
+    description="ML project",
+    author="Avnish yadav", 
+    packages=find_packages(),
+    license="MIT"
+)
